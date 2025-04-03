168acefdb38d8031d336e77b7ed1a7a16a99d0a1
Yisus Florez <juniniprogramador@outlook.com>
2025-04-03 00:18:27 -0500

    feat: added base implementation of mfe with native federation

--- shell/src/app/app.routes.ts
+++ shell/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/native-federation';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'remotea',
+    loadComponent: () =>
+      loadRemoteModule('mfea', './Component').then((m) => m.AppComponent),
+  },
+  {
+    path: 'remoteb',
+    loadComponent: () =>
+      loadRemoteModule('mfeb', './Component').then((m) => m.AppComponent),
+  },
+  {
+    path: 'remotec',
+    loadComponent: () =>
+      loadRemoteModule('mfec', './Component').then((m) => m.AppComponent),
+  }
+];
